bf8f43508ac8cb6ecf23291374085334d3afbcec
Seth McCullough <seth@endevrs.dev>
2023-01-04 08:27:28 -0500

    Add custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import "./globals.css";
+import { Inter } from "@next/font/google";
+
+const inter = Inter();
 
 export default function RootLayout({
   children,
@@ -6,11 +9,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={inter.className}>
       <head />
       <body>{children}</body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold grid h-screen place-items-center text-center">
+      Welcome to the SaaS Starter Pack!
+    </h1>
+  );
 }
