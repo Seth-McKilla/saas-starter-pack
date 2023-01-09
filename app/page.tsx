a1a8980b509f2692d39e138130549b73adf5745d
Seth McCullough <seth@endevrs.dev>
2023-01-09 13:45:41 -0500

    Consume mongodb atlas data in app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import clientPromise from "@/lib/mongodb/client";
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <h1 className="grid h-screen text-3xl font-bold text-center place-items-center">
-      Welcome to the SaaS Starter Pack!
+      {data[0]?.message}
     </h1>
   );
+}
+
+async function getData() {
+  const client = await clientPromise;
+  const db = client.db("main");
+  const data = await db.collection("test").find({}).toArray();
+  return data;
 }
